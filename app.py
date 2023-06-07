b640c9cf130e66c600f6d8a1cfb172cd3fcfce31
Mlindens <maglinden@gmail.com>
2023-06-07 16:30:35 -0400

    small SQL updates

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, url_for
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+# loads variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = "mjuwamWAAW"
+
+# gets variable from env
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
 
 
 @app.route("/")
@@ -12,8 +22,24 @@ def index():
 
 @app.route("/", methods=["POST", "GET"])
 def add():
-    flash(f"Hi, {str(request.form['name_input'])}")
-    return render_template("index.html")
+    if request.method == 'POST':
+        product_name = request.form['product_name']
+        quantity = int(request.form['quantity'])
+        size = request.form['size']
+        exp_date = request.form['exp-date']
+
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute('INSERT INTO pantry (name, quantity, size, exp_date)'
+                               'VALUES (%s %s %s %s',
+                               (product_name, quantity, size, exp_date))
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    return render_template()
+
+
 
 
 
